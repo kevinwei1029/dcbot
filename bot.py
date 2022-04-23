5e806ae95514be76bdc113b050e85f50de255f02
dongroy <rdong134@gmail.com>
2022-04-23 17:52:09 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,21 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='/')
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='/',intents = intents)
 
 @bot.event
 async def on_ready():
     print("testing")
+
+#@bot.event
+#async def on_guild_emojis_update():
+#    channel = bot.get_channel(966727218048925699)
+#    await channel.send("有新表情喔!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Ping: {round(bot.latency*1000)}(毫秒)')
 
 bot.run('OTY2NzI5NTQyOTU1ODM5NTQ5.YmF_DA.Wp4-l3BJIO8XesXb7TJT8SiU_3E')
