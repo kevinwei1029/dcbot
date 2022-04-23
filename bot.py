9ecf8dada4054bb9c8cb7a84b9266145719f3f3a
dongroy <rdong134@gmail.com>
2022-04-23 16:26:55 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='>')
+bot = commands.Bot(command_prefix='/')
+
+@bot.event
+async def on_ready():
+    print("testing")
+
+bot.run('OTY2NzI5NTQyOTU1ODM5NTQ5.YmF_DA.Wp4-l3BJIO8XesXb7TJT8SiU_3E')
