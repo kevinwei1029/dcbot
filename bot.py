adef948fa0041f81a10aff6795e9253e5b385ac0
dongroy <71483617+dongroy@users.noreply.github.com>
2022-05-08 01:56:36 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
-from ast import For, If
 import discord
 from discord.ext import commands
 import json
-import random
 import os
+import keep_alive
 
 with open('setting.json', mode='r', encoding='utf8') as jfile: #'r'=read(讀取)
     jdata = json.load(jfile)
@@ -34,20 +33,12 @@ async def reload(ctx, ext):
     bot.reload_extension(f'指令.{ext}')
     await ctx.send(f'Reloaded {ext} done.')
 
-# @bot.command()#上傳指令分類
-# async def load(ctx, ext):
-#    if ctx.message.author.id == int(jdata['OWNER']):
-#        bot.load_extension(f'指令.{ext}')
-#        await ctx.send(f'Loaded {ext} done.')
-#    else:
-#        await ctx.send('你沒有權限使用此指令')
 
 
-
-
-for Filename in os.listdir('./指令'):#./為相對路徑，也可直接給完整路徑
+for Filename in os.listdir('./指令'):
     if Filename.endswith('.py'):
         bot.load_extension(f'指令.{Filename[:-3]}')
 
 if __name__ == "__main__":
-    bot.run(jdata['TOKEN'])
+    keep_alive.keep_alive()
+    bot.run(os.environ['token'])#replit
